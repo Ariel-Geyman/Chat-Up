3cfddb86b053bd2028f05e1d6ff05f71ad4cfe1f
Ariel Geyman <ariel.geyman@gmail.com>
2022-04-06 20:12:36 +0300

    Added the signUp function to the signUp page #1

--- Chat-Up-Project/login.js
+++ Chat-Up-Project/login.js
@@ -14,9 +14,33 @@ function signIn() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let json = JSON.parse(xhr.responseText);
-            console.log(json.email + ", " + json.password);
+            console.log(json);
         }
     };
 
     xhr.send();
+}
+
+function signUp() {
+        // Getting the username, password and Email from the input elements
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        let email = document.getElementById("email").value;
+    
+        // Creating an HTTP request using JSON
+        let xhr = new XMLHttpRequest();
+        let url = "locaclhost::6969";
+    
+        xhr.open("POST", url, true);
+    
+        xhr.setRequestHeader("Content-Type", "application/json");
+    
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let json = JSON.parse(xhr.responseText);
+                console.log(json);
+            }
+        };
+        let data = JSON.stringify({"username": username, "password": password, "email": email});
+        xhr.send(data);
 }
